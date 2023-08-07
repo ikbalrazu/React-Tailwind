d13cd6648248d6771be7d318c4167042398976c6
ikbalrazu <iqbalraju123@gmail.com>
2023-08-07 20:14:47 +0600

    studing on tailwind

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">
-      Hello world!
-      </h1>
+    <div className="wrapper">
+      <nav>
+        <div className='logo'>Tailwind</div>
+        <ul>
+          <li>Install</li>
+          <li>Docs</li>
+        </ul>
+      </nav>
+      <div className='hero'>
+        <h1 className='header'>
+          Tailwind CSS makes styling React component easier!
+        </h1>
+        <button className='btn'>Get Started</button>
+      </div>
     </div>
   );
 }
